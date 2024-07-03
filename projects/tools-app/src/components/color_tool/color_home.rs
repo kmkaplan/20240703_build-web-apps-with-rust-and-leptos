c41fb284b1b4a26eeb75ef8e1b457df44097ba36
kmkaplan <kmkaplan@users.noreply.github.com>
2024-07-03 18:50:55 +0000

    End of class

--- projects/tools-app/src/components/color_tool/color_home.rs
+++ projects/tools-app/src/components/color_tool/color_home.rs
@@ -1,11 +1,20 @@
 use leptos::*;
-
+use crate::repositories::color_repository::{all_colors, AppendColor, RemoveColor};
+use crate::components::color_tool::{color_form::ColorForm, color_list::ColorList};
 #[component]
 pub fn ColorHome() -> impl IntoView {
+    let append_color = create_server_action::<AppendColor>();
+    let remove_color = create_server_action::<RemoveColor>();
+    let color_resource = create_resource(
+        move || (append_color.version().get(), remove_color.version().get()),
+        |_| all_colors(),
+    );
     view! {
         <div>
             <header>
                 <h2>ColorTool</h2>
+                <ColorList color_resource=color_resource remove_color=remove_color />
+                <ColorForm append_color=append_color /> 
             </header>
         </div>
     }
--- projects/tools-app/src/components/mod.rs
+++ projects/tools-app/src/components/mod.rs
@@ -1,2 +1,3 @@
+pub mod error_fallback;
 pub mod parent_child;
 pub mod color_tool;
