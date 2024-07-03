5ff41ff018a9ad0d442fcc472d51022d85fc6351
kmkaplan <kmkaplan@users.noreply.github.com>
2024-07-03 16:27:17 +0000

    Child callback component

--- projects/tools-app/src/components/parent_child/parent_child_callback.rs
+++ projects/tools-app/src/components/parent_child/parent_child_callback.rs
@@ -1,5 +1,23 @@
+use ev::MouseEvent;
 use leptos::*;
 
+#[component]
+pub fn Child(
+    counter: ReadSignal<i16>,
+    #[prop(into)] on_increment: Callback<MouseEvent>,
+    #[prop(into)] on_decrement: Callback<MouseEvent>,
+) -> impl IntoView {
+    view! {
+            <div style="border: 1px solid black; margin: 4px">
+                <h3>Child callback</h3>
+                <p>"Counter: " {counter}</p>
+                <div>
+                    <button type="button" on:click=on_increment>Parent +1</button>
+                    <button type="button" on:click=on_decrement>Parent -1</button>
+                </div>
+            </div>
+    }
+}
 #[component]
 pub fn Parent() -> impl IntoView {
     let (counter, set_counter) = create_signal::<i16>(0);
@@ -7,15 +25,6 @@ pub fn Parent() -> impl IntoView {
     let decrement_counter = move |_| set_counter.update(|c| *c -= 1);
 
     view! {
-        <div>
-            <div style="border: 1px solid black; margin: 4px">
-                <h3>Parent child callback</h3>
-                <p>"Counter: " {counter}</p>
-                <div>
-                    <button type="button" on:click=increment_counter>Parent +1</button>
-                    <button type="button" on:click=decrement_counter>Parent -1</button>
-                </div>
-            </div>
-        </div>
+        <Child counter=counter on_increment=increment_counter on_decrement=decrement_counter/>
     }
 }
